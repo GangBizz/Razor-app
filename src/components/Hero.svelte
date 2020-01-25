dcf5bf820a806036db971c24bbeb75928a3adc3b
Sahil Khan <sahilkhan786111@gmail.com>
2020-01-25 06:10:04 +0000

    inital commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,9 @@
+<div class="hero">
+  <div class="banner">
+    <h1>shave, sharp, style</h1>
+    <p>Embrace your sharpness - we do</p>
+    <slot>
+      <!-- optional fallback -->
+    </slot>
+  </div>
+</div>
